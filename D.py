a0093bf9c5fa75b6de1485ab18e6c76bc858d733
shiv4920 <jaybodhkumar4920@gmail.com>
2026-06-25 01:37:22 +0530

    Add files via upload

---
+++ D.py
@@ -0,0 +1,18 @@
+# Dictionary
+Dict={
+    "name":"D",
+    "age":  20,
+    "subjects":["Math", "Physics", "Chemistry"],
+    "is adult": True,
+    12.99:94.4
+}
+print(Dict)
+print(type(Dict))
+print(Dict["name"])
+print(Dict["age"])
+print(Dict["subjects"])
+print(Dict["is adult"]) 
+Dict["age"]=21
+Dict["name"]="ShivShankar"
+Dict["Surnmae"]="Shankar"
+print(Dict)
